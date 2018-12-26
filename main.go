576adffd670e7edb855ae7b62d8e5f3e9f1723d3
flevanti <frenke@frenke.it>
2018-12-26 14:06:53 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package isAwsLambda
+
+import "os"
+
+var isLambda bool
+var isDocker bool
+var isAWS bool
+var isInitialised bool
+
+func IsItLambda() bool {
+	initialise()
+	return isLambda
+}
+
+func IsItDocker() bool {
+	initialise()
+	return isDocker
+}
+
+func IsItAWS() bool {
+	initialise()
+	return isAWS
+}
+
+func IsItInitialised() bool {
+	return isInitialised
+}
+
+func initialise() {
+	if isInitialised {
+		return
+	}
+
+	//default value
+	isLambda = false
+	isDocker = false
+	isAWS = false
+
+	if len(os.Getenv("AWS_REGION")) != 0 {
+		isLambda = true
+	}
+	//even if we are in an AWS/LAMBDA environment, it could be a docker container...
+	//so let's use another env var to understand if docker
+	//after comparing docker lambda and AWS lambda I noticed that AWS_SESSION_TOKEN env var is (for the moment) only available in AWS
+	if isLambda && len(os.Getenv("AWS_SESSION_TOKEN")) == 0 {
+		isDocker = true
+	}
+	//Try to understand if we are running in AWS
+	isAWS = isLambda && !isDocker
+
+	isInitialised = true
+
+}
